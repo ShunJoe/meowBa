d437dde8c9aa46145c42c763427721ac102732e6
nbz686 <nbz686@ku.dk>
2023-04-19 10:21:57 +0200

    test flertrådet

--- file_cdtest.sh
+++ file_cdtest.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 j=10000
 for ((i = 1; i <= j; i++)) do (
-	mkdir "dir${i}"
-	> "file${i}"
-	sleep 1
-	rmdir "dir${i}"
-	rm "file${i}"
+	mkdir "dir${i}" & mkdir "dir${i}copy"
+	> "file${i}" & > "file${i}copy"
+	rmdir "dir${i}" & rm "dir${i}copy"
+	rm "file${i}" & rm "file${i}copy"
 )
+wait
 done
