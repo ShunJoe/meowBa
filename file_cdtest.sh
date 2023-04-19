2d9ad9645d23aee630e32cfc5f0214804a3511b1
Shun <shunjoe.alex@live.dk>
2023-04-19 09:40:32 +0200

    test changes

--- file_cdtest.sh
+++ file_cdtest.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
-j=4
+j=10000
 for ((i = 1; i <= j; i++)) do (
-	mkdir "dir${i}" & mkdir "dir${i}${j}"
-	> "file${i}" & > "file${i}${j}"
-	sleep 1 & sleep 1
-	rmdir "dir${i}" & rmdir "dir${i}${j}"
-	rm "file${i}" & rm "file${i}${j}"
+	mkdir "dir${i}"
+	> "file${i}"
+	sleep 1
+	rmdir "dir${i}"
+	rm "file${i}"
 )
-wait
 done
