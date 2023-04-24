11c85dbb60bcfcbab058e7e35902d8cd8a5a389c
nbz686 <nbz686@ku.dk>
2023-04-24 13:28:57 +0200

    clean up

---
+++ faNotify/simple_faNMount.py
@@ -0,0 +1,28 @@
+import select
+import os
+import pyfanotify as fan
+
+if __name__ == '__main__':
+    fanot = fan.Fanotify(init_fid=True)
+    path = "/mnt/test"
+    fanot.mark(path, ev_types=fan.FAN_CREATE | fan.FAN_MOVED_FROM | fan.FAN_OPEN | fan.FAN_MODIFY | fan.FAN_ONDIR , is_type='mt')
+    fanot.start()
+
+    cli = fan.FanotifyClient(fanot, path_pattern=path+'/*')
+    poll = select.poll()
+    poll.register(cli.sock.fileno(), select.POLLIN)
+    id = 0
+    try:
+        while poll.poll():
+            x = {}
+            for i in cli.get_events():
+                i.ev_types = fan.evt_to_str(i.ev_types)
+                id = id +1 
+                x.setdefault(i.path, [i.pid, i.ev_types, id])
+            if x:
+                print(x)
+    except:
+        print('STOP')
+
+    cli.close()
+    fanot.stop()
